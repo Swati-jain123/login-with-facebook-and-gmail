08a7b1112b3271775a95aac524e1dff2e3e9972d
Login with facebook ang gmail <swati2003jain@gmail.com>
2022-07-04 12:02:58 +0530

    commit changes

---
+++ hello.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Center Align Buttons in Bootstrap</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style>
+    /* Style to center grid column */
+    
+</style>
+</head>
+<body>
+    <div class="container my-3 bg-light">
+        <div class="col-md-12 text-center">
+           <a href="index.php"> <button type="button" href="index.php" class="btn btn-primary btn-lg btn-block">Login With Google </button> </a>  <br> 
+
+           <a href="index1.php"> <button type="button" class="btn btn-secondary btn-lg btn-block" style="margin-top:150px">Login with Facebook </button> </a>
+
+           </div>
+    </div>
+</body>
+</html>
